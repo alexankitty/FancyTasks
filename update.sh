92d7c2a5593a75a5b40f4a624701f93a6d2738f1
Alexandra <alexankitty@gmail.com>
2023-08-23 10:53:59 -0600

    update scripts

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-kpackagetool5 -u $SCRIPT_DIR
+kpackagetool5 -u $SCRIPT_DIR/package
 kquitapp5 plasmashell
 kstart5 plasmashell
 cp ./FancyTasks.png ~/.local/share/icons/hicolor/256x256/apps/FancyTasks.png
