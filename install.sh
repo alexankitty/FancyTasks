265ed34a488a5088e183f5fe75b3a7fb7fef1c18
Alexandra <alexankitty@gmail.com>
2023-08-23 18:13:44 -0600

    Update scripts

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+sh $SCRIPT_DIR/package/translate/merge
+sh $SCRIPT_DIR/package/translate/build
 kpackagetool5 -i $SCRIPT_DIR/package
 mkdir -p ~/.local/share/icons/hicolor/256x256/apps/
 cp ./FancyTasks.png ~/.local/share/icons/hicolor/256x256/apps/FancyTasks.png
--- update.sh
+++ update.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR/package/translate/
+sh ./merge
+sh ./build
+cd $SCRIPT_DIR
 kpackagetool5 -u $SCRIPT_DIR/package
-kquitapp5 plasmashell
+killall plasmashell
 kstart5 plasmashell
 cp ./FancyTasks.png ~/.local/share/icons/hicolor/256x256/apps/FancyTasks.png
