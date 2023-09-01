1c8eb15c78eb69419db9b71b991eeb25794451db
Alexandra <alexankitty@gmail.com>
2023-09-01 00:44:03 -0600

    restore icon installation

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 sh $SCRIPT_DIR/build.sh
 kpackagetool5 -i $SCRIPT_DIR/release/FancyTasks.tar.gz
+cp ./package/FancyTasks.png ~/.local/share/icons/hicolor/256x256/apps/FancyTasks.png
