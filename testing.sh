91295288057b9f90558877f510521f64895f819f
Alexandra <alexankitty@gmail.com>
2023-09-14 06:41:55 -0600

    add testing script

---
+++ testing.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+sh $SCRIPT_DIR/build.sh
+kpackagetool5 -u $SCRIPT_DIR/release/FancyTasks.tar.gz
+QML_DISABLE_DISK_CACHE=true plasmoidwindowed -a alexankitty.fancytasks
